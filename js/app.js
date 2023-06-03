3492b954115caf7428d25e159433f59eb358d375
Atoche Pascual <jmatochepascual@gmail.com>
2023-06-03 13:35:17 -1000

    [ADD] spinner

--- js/app.js
+++ js/app.js
@@ -59,15 +59,21 @@ const consultarApi = (ciudad, pais) => {
   const appId = "bdb137a7be80a9e65889b73b0a258937";
   const URL = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
 
+  spinner();
+
   fetch(URL)
     .then(respuesta => respuesta.json())
     .then(datos => {
+      limpiarHtml();
+
       if (datos.cod === '404') {
         mostrarAlerta('Ciudad no encontrada');
         return;
       }
 
       mostrarClima(datos);
+
+      formulario.reset();
     })
 };
 
@@ -75,18 +81,31 @@ const consultarApi = (ciudad, pais) => {
 
 // * Muestr el clima en pantalla
 const mostrarClima = (datos) => {
-  const { main: { temp, temp_max, temp_min } } = datos;
+  const { name, main: { temp, temp_max, temp_min } } = datos;
   const temperatura = convertirCentigrados(temp);
-  const temperaturaMax = convertirCentigrados(temp);
-  const temperaturaMin = convertirCentigrados(temp);
-  const tenperaturaHtml = document.createElement('p');
-  const resultadoDiv = document.createElement('div');
+  const temperaturaMax = convertirCentigrados(temp_max);
+  const temperaturaMin = convertirCentigrados(temp_min);
 
+  const tenperaturaHtml = document.createElement('p');
   tenperaturaHtml.innerHTML = `${temperatura} &#8451;`;
   tenperaturaHtml.classList.add('font-bold', 'text-6xl');
 
+  const resultadoDiv = document.createElement('div');
   resultadoDiv.classList.add('text-center', 'text-white');
-  resultadoDiv.appendChild(tenperaturaHtml);
+
+  const temperatutaMaxHtml = document.createElement('p');
+  temperatutaMaxHtml.innerHTML = `Max: ${temperaturaMax} &#8451;`;
+  temperatutaMaxHtml.classList.add('text-xl')
+
+  const temperatutaMinHtml = document.createElement('p');
+  temperatutaMinHtml.innerHTML = `Min: ${temperaturaMin} &#8451;`;
+  temperatutaMinHtml.classList.add('text-xl')
+
+  const nombreCiudad = document.createElement('p');
+  nombreCiudad.textContent = `Clima en ${name}`;
+  nombreCiudad.classList.add('font-bold', 'text-2xl')
+
+  resultadoDiv.append(nombreCiudad, tenperaturaHtml, temperatutaMaxHtml, temperatutaMinHtml);
 
   resultado.appendChild(resultadoDiv);
 };
@@ -94,7 +113,7 @@ const mostrarClima = (datos) => {
 
 
 // * Convierte a grados centigrados
-const convertirCentigrados = (temperatura) => (temperatura - 273.15).toFixed(2);
+const convertirCentigrados = (temperatura) => parseInt(temperatura - 273.15);
 
 
 
@@ -103,4 +122,31 @@ const limpiarHtml = () => {
   while (resultado.firstChild) {
     resultado.firstChild.remove();
   }
+};
+
+
+
+// * Muestra un spinner
+const spinner = () => {
+
+  limpiarHtml();
+
+  const divSpinner = document.createElement('div');
+  divSpinner.classList.add('sk-fading-circle');
+  divSpinner.innerHTML = `
+  <div class="sk-circle1 sk-circle"></div>
+  <div class="sk-circle2 sk-circle"></div>
+  <div class="sk-circle3 sk-circle"></div>
+  <div class="sk-circle4 sk-circle"></div>
+  <div class="sk-circle5 sk-circle"></div>
+  <div class="sk-circle6 sk-circle"></div>
+  <div class="sk-circle7 sk-circle"></div>
+  <div class="sk-circle8 sk-circle"></div>
+  <div class="sk-circle9 sk-circle"></div>
+  <div class="sk-circle10 sk-circle"></div>
+  <div class="sk-circle11 sk-circle"></div>
+  <div class="sk-circle12 sk-circle"></div>
+  `;
+
+  resultado.appendChild(divSpinner);
 };
