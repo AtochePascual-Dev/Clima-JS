76a06dbd3422ddf085116f2cb265607bd6a2f750
Atoche Pascual <jmatochepascual@gmail.com>
2023-06-03 21:53:20 -1000

    Updates

---
+++ README.md
@@ -0,0 +1,26 @@
+# CLIMA
+
+## Descripción
+
+El proyecto consiste en una aplicación web para consultar el clima de una ciudad y país específicos. La interfaz contiene un formulario con campos de entrada para ingresar la ciudad y el país. Al enviar el formulario, se realiza una consulta a la API de OpenWeatherMap para obtener los datos del clima. Si la ciudad no es encontrada, se muestra una alerta. En caso contrario, se muestra la temperatura actual, la temperatura máxima y mínima en grados centigrados. La interfaz también incluye un spinner de carga mientras se realiza la consulta. El código utiliza JavaScript para manejar los eventos del formulario.
+
+## Instrucciones de uso
+
+Para acceder a la página, simplemente abra el archivo index.html en su navegador web
+
+## Tecnologías utilizadas
+
+![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)
+![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)
+![TailwindCSS](https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white)
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+
+</br>
+
+## Contribución
+
+Si deseas contribuir al proyecto, por favor, haz un fork del repositorio y crea una rama con tus cambios. Luego, envía una solicitud de extracción para que podamos revisar tus cambios antes de fusionarlos en la rama principal.
+
+# preview
+
+![](https://i.imgur.com/YhoNaXR.png)
---
+++ js/app.js
@@ -0,0 +1,153 @@
+// * VARIABLES
+const container = document.querySelector('.container');
+const resultado = document.querySelector('#resultado');
+const formulario = document.querySelector('#formulario');
+
+
+
+// * EVENTOS
+// * Cuando el documento esta listo
+document.addEventListener('DOMContentLoaded', () => {
+  formulario.addEventListener('submit', buscarClima);
+});
+
+
+
+// * FUNCIONES
+// * Busca un clima
+const buscarClima = (event) => {
+  event.preventDefault();
+
+  // Obtenemos los valores de los imputs
+  const ciudad = document.querySelector('#ciudad').value.trim();
+  const pais = document.querySelector('#pais').value.trim();
+
+  // validamos si la infomacion contiene vacio
+  if ([ciudad, pais].includes('')) {
+    mostrarAlerta('Todos los campos son obligatorios');
+    return;
+  }
+
+  consultarApi(ciudad, pais);
+
+};
+
+
+
+// * Muestra un mensaje en pantalla
+const mostrarAlerta = (mensaje) => {
+  const existeAlerta = document.querySelector('.alerta');
+
+  if (!existeAlerta) {
+    const alerta = document.createElement('div');
+    alerta.textContent = mensaje;
+    alerta.className = "bg-red-100 border-red-400 text-red-700 px-4 py-3 rounder max-w-md mx-auto mt-6 text-center font-bold alerta";
+
+    container.appendChild(alerta);
+
+    // eliminamos la alerta
+    setTimeout(() => {
+      alerta.remove();
+    }, 2000);
+  }
+};
+
+
+
+// * Realiza una consulta a la API
+const consultarApi = (ciudad, pais) => {
+  const appId = "bdb137a7be80a9e65889b73b0a258937";
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+
+  spinner();
+
+  fetch(URL)
+    .then(respuesta => respuesta.json())
+    .then(datos => {
+      limpiarHtml();
+
+      if (datos.cod === '404') {
+        mostrarAlerta('Ciudad no encontrada');
+        formulario.reset();
+        return;
+      }
+
+      mostrarClima(datos);
+
+      formulario.reset();
+    })
+};
+
+
+
+// * Muestr el clima en pantalla
+const mostrarClima = (datos) => {
+  const { name, main: { temp, temp_max, temp_min } } = datos;
+  const temperatura = convertirCentigrados(temp);
+  const temperaturaMax = convertirCentigrados(temp_max);
+  const temperaturaMin = convertirCentigrados(temp_min);
+
+  const tenperaturaHtml = document.createElement('p');
+  tenperaturaHtml.innerHTML = `${temperatura} &#8451;`;
+  tenperaturaHtml.classList.add('font-bold', 'text-6xl');
+
+  const resultadoDiv = document.createElement('div');
+  resultadoDiv.classList.add('text-center', 'text-white');
+
+  const temperatutaMaxHtml = document.createElement('p');
+  temperatutaMaxHtml.innerHTML = `Max: ${temperaturaMax} &#8451;`;
+  temperatutaMaxHtml.classList.add('text-xl')
+
+  const temperatutaMinHtml = document.createElement('p');
+  temperatutaMinHtml.innerHTML = `Min: ${temperaturaMin} &#8451;`;
+  temperatutaMinHtml.classList.add('text-xl')
+
+  const nombreCiudad = document.createElement('p');
+  nombreCiudad.textContent = `Clima en ${name}`;
+  nombreCiudad.classList.add('font-bold', 'text-2xl')
+
+  resultadoDiv.append(nombreCiudad, tenperaturaHtml, temperatutaMaxHtml, temperatutaMinHtml);
+
+  resultado.appendChild(resultadoDiv);
+};
+
+
+
+// * Convierte a grados centigrados
+const convertirCentigrados = (temperatura) => parseInt(temperatura - 273.15);
+
+
+
+// * Limpia el html previo
+const limpiarHtml = () => {
+  while (resultado.firstChild) {
+    resultado.firstChild.remove();
+  }
+};
+
+
+
+// * Muestra un spinner
+const spinner = () => {
+
+  limpiarHtml();
+
+  const divSpinner = document.createElement('div');
+  divSpinner.classList.add('sk-fading-circle');
+  divSpinner.innerHTML = `
+  <div class="sk-circle1 sk-circle"></div>
+  <div class="sk-circle2 sk-circle"></div>
+  <div class="sk-circle3 sk-circle"></div>
+  <div class="sk-circle4 sk-circle"></div>
+  <div class="sk-circle5 sk-circle"></div>
+  <div class="sk-circle6 sk-circle"></div>
+  <div class="sk-circle7 sk-circle"></div>
+  <div class="sk-circle8 sk-circle"></div>
+  <div class="sk-circle9 sk-circle"></div>
+  <div class="sk-circle10 sk-circle"></div>
+  <div class="sk-circle11 sk-circle"></div>
+  <div class="sk-circle12 sk-circle"></div>
+  `;
+
+  resultado.appendChild(divSpinner);
+};
